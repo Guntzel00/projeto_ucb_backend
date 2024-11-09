dfc4f7667f4db60d69eb5a0f6595d26bb284a54e
Guntzel00 <gustavo.guntzel@outlook.com>
2024-11-09 03:00:22 -0300

    Centro de doação fix

--- services/centroDoacaoService.js
+++ services/centroDoacaoService.js
@@ -2,30 +2,30 @@
 const CentroDoacao = require('../models/centroDoacao');
 
 const criarCentro = async (dados) => {
-    const centro = new CentroDoacao(dados);
-    return await centro.save();
+	const centro = new CentroDoacao(dados);
+	return await centro.save();
 };
 
 const listarCentros = async () => {
-    return await CentroDoacao.find();
+	return await CentroDoacao.find();
 };
 
 const buscarCentroPorId = async (id) => {
-    return await CentroDoacao.findOne({ id });
+	return await CentroDoacao.findById(id);
 };
 
 const atualizarCentro = async (id, dados) => {
-    return await CentroDoacao.findOneAndUpdate({ id }, dados, { new: true });
+	return await CentroDoacao.findOneAndUpdate({ id }, dados, { new: true });
 };
 
 const deletarCentro = async (id) => {
-    return await CentroDoacao.findOneAndDelete({ id });
+	return await CentroDoacao.findOneAndDelete({ id });
 };
 
 module.exports = {
-    criarCentro,
-    listarCentros,
-    buscarCentroPorId,
-    atualizarCentro,
-    deletarCentro
+	criarCentro,
+	listarCentros,
+	buscarCentroPorId,
+	atualizarCentro,
+	deletarCentro,
 };
